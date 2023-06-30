5c6a5e842fd36fe86a1ce6904527427673dc6b3f
Stella_Ha <sangyeeh@gmail.com>
2023-06-30 17:50:52 +0900

    :memo: 완벽한 깊은복사 Deep Copy, lodash, cloneDeep
    완벽한 깊은복사 Deep Copy, lodash, cloneDeep

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const lodash = require("lodash");
+const object1 = {
+  name: "Stella",
+  age: 25,
+};
+const lodash = require("lodash"); // lodash 모듈을 가져온다.
+const object2 = lodash.cloneDeep(object1); // lodash 모듈의 cloneDeep 메소드를 사용하여 object1을 복사한다.
+console.log(object1 === object2); // false, 동일한 내용을 가지고 있지만, 다른 메모리 주소를 가지고 있다. (완전히 독립됨)
 
-const object4 = lodash.cloneDeep(object1);
+console.log(object1); // { name: 'Stella', age: 25 }
+console.log(object2); // { name: 'Stella', age: 25 }
 
-console.log(object1 === object4);
-object4.food.us = "buger";
-console.log(object1);
-console.log(object4);
+object1.age = 21;
+console.log(object1.age === object2.age); // false
