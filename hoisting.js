e2b4625730cf2d4258247bf56907c6c940d338dc
Stella_Ha <sangyeeh@gmail.com>
2023-07-02 15:25:06 +0900

    :memo: 함수 표현식 (화살표 함수) 호이스팅 x
    함수 표현식 (화살표 함수) 호이스팅 x

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,4 @@
-// 변수 선언과 클래스 선언은 hoisting이 일어나지 않는다. 왜냐하면 변수와 클래스는 선언만 호이스팅이 되고, 초기화는 안된다.
+// 변수 선언과 클래스 선언은 hoisting이 일어나지 않는다. 왜냐하면 변수와 클래스는 선언만 호이스팅이 되고, 초기화는 나중에 실행 단계에서 처리된다.
 
 // 함수 선언은 hoisting이 일어난다.
 
@@ -12,8 +12,14 @@
 // // ReferenceError: Cannot access 'Cat' before initialization
 // class Cat {}
 
-// 3. 함수 선언
-console.log(hi()); // hi
-function hi() {
-  return "hi";
-}
+// 3. 함수 선언식
+// console.log(hi()); // hi
+// function hi() {
+//   return "hi";
+// }
+
+// 4. 함수 표현식 (화살표 함수)
+console.log(hello()); // TypeError: hello is not a function
+const hello = function () {
+  return "hello";
+};
