949575dd86c6aac69a6a715448afed9ceb8756aa
Stella_Ha <sangyeeh@gmail.com>
2023-07-13 22:53:25 +0900

    :memo: react 관련 노트 업데이트
     react 관련 노트 업데이트

--- scopeANDclosure.js
+++ scopeANDclosure.js
@@ -38,3 +38,13 @@ const incCounter = setCounter(); // 내부 함수를 통해 외부 함수 렉시
 incCounter();
 incCounter();
 incCounter();
+
+// setCounter() 함수를 호출하면, setCounter() 함수 내부에 정의된 count 변수와 increase() 함수가 생성됩니다. 그러나 setCounter() 함수는 increase() 함수를 반환하고 종료하게 됩니다.
+
+// 따라서, setCounter() 함수 자체를 호출하는 것은 내부에 정의된 increase() 함수를 실행시키지 않습니다. 단지 increase() 함수를 참조할 수 있는 값을 반환하는 것입니다.
+
+// const incCounter = setCounter();는 setCounter() 함수를 호출하여 반환된 increase() 함수를 incCounter 변수에 할당하는 것을 의미합니다. 이렇게 incCounter 변수에 할당된 increase() 함수를 호출하면, increase() 함수가 실행되고 count 변수의 값이 증가하여 출력됩니다.
+
+// 즉, incCounter()를 호출할 때마다 increase() 함수가 실행되며, 그 안에서 count 변수의 값이 증가하고 출력됩니다. incCounter()를 여러 번 호출하면 count 변수의 값이 계속 증가하게 됩니다.
+
+// 따라서, setCounter() 함수를 호출하는 것과 incCounter() 함수를 호출하는 것은 서로 다른 동작을 수행하며, incCounter()를 호출해야 increase() 함수가 실행되어 count 변수의 값이 변경되고 출력됩니다.
